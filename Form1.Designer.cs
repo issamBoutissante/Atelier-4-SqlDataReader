b2e5715da8dfd13ae3666e3b4fd44788c4223ed6
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-11-10 11:30:35 +0100

    datareader object

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Atelier__Lobjet_DataReader_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Num_Avion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Marque = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type_Avion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Afficher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Num_Avion,
+            this.Marque,
+            this.Type_Avion});
+            this.dataGridView1.Location = new System.Drawing.Point(130, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 248);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Num_Avion
+            // 
+            this.Num_Avion.HeaderText = "Num Avion";
+            this.Num_Avion.Name = "Num_Avion";
+            this.Num_Avion.ReadOnly = true;
+            // 
+            // Marque
+            // 
+            this.Marque.HeaderText = "Marque";
+            this.Marque.Name = "Marque";
+            this.Marque.ReadOnly = true;
+            // 
+            // Type_Avion
+            // 
+            this.Type_Avion.HeaderText = "Type Avion";
+            this.Type_Avion.Name = "Type_Avion";
+            this.Type_Avion.ReadOnly = true;
+            // 
+            // Afficher
+            // 
+            this.Afficher.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Afficher.Location = new System.Drawing.Point(538, 160);
+            this.Afficher.Name = "Afficher";
+            this.Afficher.Size = new System.Drawing.Size(125, 53);
+            this.Afficher.TabIndex = 1;
+            this.Afficher.Text = "Afficher";
+            this.Afficher.UseVisualStyleBackColor = true;
+            this.Afficher.Click += new System.EventHandler(this.Afficher_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(199, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Information Avions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 346);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Afficher);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Avion";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num_Avion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marque;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type_Avion;
+        private System.Windows.Forms.Button Afficher;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Atelier__Lobjet_DataReader_
+{
+    public partial class Form1 : Form
+    {
+        static string conn_string = ConfigurationManager.ConnectionStrings["mon_db"].ConnectionString;
+        SqlConnection connection;
+        SqlCommand command ;
+        SqlDataReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+           
+              
+        }
+
+        private void Afficher_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            try
+            {
+                connection = new SqlConnection(conn_string);
+                command = new SqlCommand("select * from avion;", connection);
+                connection.Open();
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader.GetInt16(0).ToString(), reader.GetString(1), reader.GetString(2));
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("erreur : " + ex.Message);
+            }
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
